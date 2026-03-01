6e59314383e219bcd317ec21a5ec73023a653465
Mikhail Kornilovich <kornilovichmihail@gmail.com>
2026-03-01 19:57:52 +0300

    [supervisor] add stdout and stderr support

--- src/supervisor.rs
+++ src/supervisor.rs
@@ -1,6 +1,9 @@
-use std::time::Duration;
+use std::{process::Stdio, time::Duration};
 
-use tokio::process::Command;
+use tokio::{
+    io::{AsyncBufReadExt, BufReader},
+    process::Command,
+};
 
 use crate::config::Service;
 
@@ -8,10 +11,42 @@ pub async fn run_service(service: Service) {
     loop {
         println!("[{}] starting...", service.name);
 
-        let status = Command::new(&service.command)
+        let mut child = Command::new(&service.command)
             .args(&service.args)
-            .status()
-            .await;
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn();
+
+        let mut child = match child {
+            Ok(c) => c,
+            Err(e) => {
+                println!("[{}] failed to spawn: {}", service.name, e);
+                tokio::time::sleep(Duration::from_secs(2)).await;
+                continue;
+            }
+        };
+
+        if let Some(stdout) = child.stdout.take() {
+            let name = service.name.clone();
+            tokio::spawn(async move {
+                let mut lines = BufReader::new(stdout).lines();
+                while let Ok(Some(line)) = lines.next_line().await {
+                    println!("[{name}] {line}");
+                }
+            });
+        }
+
+        if let Some(stderr) = child.stderr.take() {
+            let name = service.name.clone();
+            tokio::spawn(async move {
+                let mut lines = BufReader::new(stderr).lines();
+                while let Ok(Some(line)) = lines.next_line().await {
+                    eprintln!("[{name}] {line}");
+                }
+            });
+        }
+
+        let status = child.wait().await;
 
         match status {
             Ok(s) => println!("[{}] exited with {}", service.name, s),
@@ -22,10 +57,6 @@ pub async fn run_service(service: Service) {
             "always" => {
                 println!("[{}] restarting in 2s...", service.name);
                 tokio::time::sleep(Duration::from_secs(2)).await;
-            }
-            "never" => {
-                println!("[{}] not restarting", service.name);
-                return;
             }
             _ => return,
         }
